f20384d4ef7bf9f365ea09274f16af4ce715678b
Dexus <j.choque@outlook.com>
2024-05-11 00:27:34 -0400

    Actualizacion de mi trabajo
    Ordené y puse unos div acorde a cada seccion del formulario, agregué pequeñas modificaciones a el css, al igual que declaré variables en javascript
    eso po jefe

---
+++ assets/JS/validacionRegistro.js
@@ -0,0 +1,7 @@
+//variables
+var nombre = document.getElementById("nombre").value;
+var apellidos = document.getElementById("apellidos").value;
+var genero = document.getElementById("genero").value;
+var email = document.getElementById("email").value;
+var apodo = document.getElementById("apodo").value;
+var fNac = document.getElementById("fNac").value;
